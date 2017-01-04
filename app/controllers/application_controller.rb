021e567ec9bf05d83365587bc348541df5021913
Marcelo Boeira <me@marceloboeira.com>
2017-01-04 06:31:08 +0100

    style: Omit unused exception assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  rescue_from Mongoid::Errors::DocumentNotFound do |exception|
+  rescue_from Mongoid::Errors::DocumentNotFound do
     head :not_found
   end
 end
