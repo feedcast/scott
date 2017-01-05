8a0a55f6b0eb9df049fe6bd4cf242e9dda67a64b
Marcelo Boeira <me@marceloboeira.com>
2017-01-05 03:06:51 +0100

    feature: Add root page index AB test

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,5 +1,14 @@
 class ChannelsController < ApplicationController
   def show
     @channel = Channel.find(params[:channel])
+    ab_finished(:channel_sorting_strategy)
+  end
+
+  def list
+    ab_test(:channel_sorting_strategy, "synchronized_at", "created_at", "title") do |strategy|
+      field = strategy.to_sym
+
+      @channels = Channel.all.order_by(field => :desc)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount Split::Dashboard => "/admin/experiments", as: "split_dashboard"
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
 
-  get "/:channel", to: "channels#show"
-  get "/:channel/:episode", to: "episodes#show"
+  root "channels#list"
+  get "/:channel", to: "channels#show", as: "channel"
+  get "/:channel/:episode", to: "episodes#show", as: "episode"
 end
