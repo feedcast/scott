4e219743b4fdb80871a7ae89db5843119f576510
Joao C Vieira <joaocv3@gmail.com>
2017-01-06 21:07:37 -0200

    feature: Add HTTP authentication to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  http_basic_authenticate_with name: ENV["FEEDCAST_HTTP_AUTH_USER"], password: ENV["FEEDCAST_HTTP_AUTH_PASSWORD"] if Rails.env.beta?
+
   rescue_from Mongoid::Errors::DocumentNotFound do
     head :not_found
   end
