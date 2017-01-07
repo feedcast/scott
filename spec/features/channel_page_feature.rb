70ca002a2c1b7b81e88a1fa5574c8afa5e4da731
Marcelo Boeira <me@marceloboeira.com>
2017-01-07 03:38:18 +0100

    test: Add simple scenario for channel's page

--- spec/features/channel_page_feature.rb
+++ spec/features/channel_page_feature.rb
@@ -1,11 +1,31 @@
 require "rails_helper"
 
 RSpec.feature "Channels", type: :feature do
-  let(:channel) { Fabricate(:channel) }
+  scenario "display channel's information and list of episodes" do
+    given_a_channel
+    when_i_visit_the_channel_page
+    then_i_see_the_channel_title
+    and_the_list_of_episodes
+  end
 
-  scenario "user access a channel over the root route by slug" do
-    visit "/#{channel.slug}"
+  private
 
-    expect(page).to have_text(channel.title)
+  def given_a_channel
+    @channel = Fabricate(:channel_with_episodes)
+  end
+
+  def when_i_visit_the_channel_page
+    visit "/#{@channel.slug}"
+    @page = page
+  end
+
+  def then_i_see_the_channel_title
+    expect(@page).to have_text(@channel.title)
+  end
+
+  def and_the_list_of_episodes
+    @channel.episodes.each do |episode|
+      expect(@page).to have_text(episode.title)
+    end
   end
 end
