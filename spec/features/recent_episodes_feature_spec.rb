5e6c607989dc84eb952f99841a52d297cd2097d5
Marcelo Boeira <me@marceloboeira.com>
2017-01-07 21:11:41 +0100

    refator: Rename podcast class to episode

--- spec/features/recent_episodes_feature_spec.rb
+++ spec/features/recent_episodes_feature_spec.rb
@@ -23,10 +23,10 @@ RSpec.feature "Recent Episodes", type: :feature do
   end
 
   def then_i_see_a_list_with_the_12_most_recent_episodes
-    expect(@page).to have_css(".podcast a h3", count: 12)
+    expect(@page).to have_css(".episode a h3", count: 12)
 
     @episodes.first(12).each_with_index do |episode, index|
-      expect(@page).to have_selector(".podcast a h3", text: episode.title)
+      expect(@page).to have_selector(".episode a h3", text: episode.title)
     end
   end
 end
