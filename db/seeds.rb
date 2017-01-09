56afe8893fa14b335bad0717d3de392e10eb0a02
Marcelo Boeira <me@marceloboeira.com>
2017-01-09 22:10:01 +0100

    chore: Raise exception for invalid seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Channel.create(title: "Hipsters Ponto Tech", feed_url: "http://hipsters.tech/feed/podcast/")
-Channel.create(title: "NerdCast", feed_url: "http://jovemnerd.com.br/categoria/nerdcast/feed/")
-Channel.create(title: "Não Ouvo", feed_url: "http://feeds.feedburner.com/naoouvo?format=xml")
-Channel.create(title: "Ruby on Rails Podcast", feed_url: "http://feeds.5by5.tv/rubyonrails")
-Channel.create(title: "Matando Robôs Gigantes", feed_url: "http://feed.matandorobosgigantes.com")
-
- 
+Channel.create!(
+  [
+    { title: "Hipsters Ponto Tech", feed_url: "http://hipsters.tech/feed/podcast/" },
+    { title: "NerdCast", feed_url: "http://jovemnerd.com.br/categoria/nerdcast/feed/" },
+    { title: "Não Ouvo", feed_url: "http://feeds.feedburner.com/naoouvo?format=xml" },
+    { title: "Ruby on Rails Podcast", feed_url: "http://feeds.5by5.tv/rubyonrails" },
+    { title: "Matando Robôs Gigantes", feed_url: "http://feed.matandorobosgigantes.com" },
+  ]
+)
