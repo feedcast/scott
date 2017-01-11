2d3d44020f45ec102475bcef41d1f80c9cfbcd5f
Marcelo Boeira <me@marceloboeira.com>
2017-01-11 05:41:23 +0100

    fix: Force new channels to always synchronize

--- app/operations/episode_operations/synchronize_all.rb
+++ app/operations/episode_operations/synchronize_all.rb
@@ -23,7 +23,10 @@ module EpisodeOperations
       last_episode = @channel.episodes.map(&:published_at).max
       last_item = @feed_items.map(&:publish_date).max
 
-      !last_episode.nil? && !last_item.nil? && last_item > last_episode
+      return true if last_episode.nil?
+      return false if last_item.nil?
+
+      last_item > last_episode
     end
 
     def synchronize(episode, channel)
