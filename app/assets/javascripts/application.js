9e33b977991e73d2759d1cebcdcfef6437fd1aae
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-01-14 23:55:34 -0200

    fix: Adjust panel titles and spaces

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,13 +3,3 @@
 //= require feedcast-player
 //= require_tree .
 
-try{
-var pageItems = document.querySelectorAll('.pagination li.page-item'), active;
-for(var i in pageItems){
-  if(pageItems[i].className.indexOf('active') !== -1){
-    active = i;
-    break;
-  }
-}
-pageItems[( active - 1 )].className += " show";
-} catch(e){}
