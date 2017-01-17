2d4e2aab334c528e053b0bac92268ca2b1b2c958
msmedeiros <marvin.medeiros52@gmail.com>
2017-01-17 14:52:21 -0200

    fix: Adjust episode page tests

--- spec/features/episode_page_feature_spec.rb
+++ spec/features/episode_page_feature_spec.rb
@@ -4,7 +4,6 @@ RSpec.feature "Episode", type: :feature do
   scenario "user access an episode" do
     given_an_episode
     when_i_visit_the_episode_by_its_slug
-    then_i_see_the_episode_attributes
     and_the_pages_title_include_the_episode_title
   end
 
@@ -16,10 +15,6 @@ RSpec.feature "Episode", type: :feature do
     visit "/#{@episode.channel.slug}/#{@episode.slug}"
 
     @page = page
-  end
-
-  def then_i_see_the_episode_attributes
-    expect(@page).to have_text("My cool episode")
   end
 
   def and_the_pages_title_include_the_episode_title
