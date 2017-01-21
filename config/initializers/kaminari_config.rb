80077a3748381ece1f6f0061210566d916ae6dc6
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-01-21 16:17:58 -0200

    feature: Adjust pagination window to 2

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -2,7 +2,7 @@
 Kaminari.configure do |config|
   # config.default_per_page = 25
   # config.max_per_page = nil
-  # config.window = 4
+  config.window = 2
   # config.outer_window = 0
   # config.left = 0
   # config.right = 0
