91356157ad5d13194f1218e420daa24fc9ac0675
Joao C Vieira <joaocv3@gmail.com>
2017-02-04 10:24:17 -0200

    chore: Use env variable to set episode analysis limit per run

--- app/operations/episode_operations/analyse_all.rb
+++ app/operations/episode_operations/analyse_all.rb
@@ -1,6 +1,6 @@
 module EpisodeOperations
   class AnalyseAll < FunctionalOperations::Operation
-    LIMIT_PER_RUN = 100
+    LIMIT_PER_RUN = ENV["EPISODE_ANALYSIS_LIMIT_PER_RUN"]
 
     def perform
       Episode.not_analysed.limit(LIMIT_PER_RUN).order_by(published_at: :desc).each do |episode|
