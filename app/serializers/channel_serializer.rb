6db89ec8f01bd43e6bc04be916636e09787fbd0d
Marcelo Boeira <me@marceloboeira.com>
2017-06-03 11:56:45 +0200

    feature: Add all channel attributes to the api payload

--- app/serializers/channel_serializer.rb
+++ app/serializers/channel_serializer.rb
@@ -1,3 +1,15 @@
 class ChannelSerializer < ActiveModel::Serializer
-  attributes :uuid, :slug, :title, :description
+  attributes :uuid,
+             :slug,
+             :title,
+             :description,
+             :feed_url,
+             :image_url,
+             :listed,
+             :synchronization_status,
+             :synchronization_status_message
+
+  def synchronization_status
+    object.synchronization_status.to_s
+  end
 end
