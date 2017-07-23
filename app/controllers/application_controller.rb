e09bf58843dbda64d77d6e256a63727f2a27c985
Marcelo Boeira <me@marceloboeira.com>
2017-07-23 23:23:52 +0200

    feature: Add default values for the authentication on beta env
    Its annoying to have to setup the variable on local to test...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :ensure_cookie!
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: ENV["FEEDCAST_HTTP_AUTH_USER"], password: ENV["FEEDCAST_HTTP_AUTH_PASSWORD"] if Rails.env.beta?
+  http_basic_authenticate_with name: ENV.fetch("FEEDCAST_HTTP_AUTH_USER", "admin"),
+                               password: ENV.fetch("FEEDCAST_HTTP_AUTH_PASSWORD", "admin") if Rails.env.beta?
 
   rescue_from Mongoid::Errors::DocumentNotFound do
     render "errors/404", status: :not_found
