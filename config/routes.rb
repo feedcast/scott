797977f3847036bd03b7397d23a19f7e171b4e8a
Marcelo Boeira <me@marceloboeira.com>
2017-08-05 17:38:37 +0200

    feature: Redirect domain root to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => "/admin/jobs", as: "sidekiq_dashboard"
   mount Split::Dashboard => "/admin/experiments", as: "split_dashboard"
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
+
+  root to: redirect("/admin")
 end
