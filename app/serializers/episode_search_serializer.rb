6302fb125b1fa0b6370194ad61ccf8e05a51513b
Marcelo Boeira <me@marceloboeira.com>
2017-09-15 19:20:36 +0200

    feature: Add image to episode search indexer

--- app/serializers/episode_search_serializer.rb
+++ app/serializers/episode_search_serializer.rb
@@ -2,10 +2,15 @@ class EpisodeSearchSerializer < ActiveModel::Serializer
   attributes :uuid,
              :path,
              :title,
+             :image,
              :summary,
              :description
 
   def path
     "/#{object.channel.slug}/#{object.slug}"
+  end
+
+  def image
+    object.channel.proxy_image_url
   end
 end
