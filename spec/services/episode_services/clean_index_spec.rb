f8f2a74fc8a8f44fdc1f300919b057ed333d8e36
Joao C Vieira <joaocv3@gmail.com>
2017-09-20 19:21:41 -0300

    test: Call clean index service explicitly

--- spec/services/episode_services/clean_index_spec.rb
+++ spec/services/episode_services/clean_index_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe EpisodeServices::CleanIndex do
-  let(:service) { EpisodeServices::CleanIndex.new.call }
+  let(:service) { EpisodeServices::CleanIndex.new }
   let!(:episodes) do
   [
     Fabricate(:episode, indexed_at: 1.minute.ago),
@@ -10,7 +10,7 @@ RSpec.describe EpisodeServices::CleanIndex do
   end
 
   it "runs indexer for all outdated the episodes" do
-    service
+    service.call
 
     expect(Episode.all.map { |i| i.indexed_at }).to all(be_nil)
   end
